3bb9b4c6792d133ada13b91633d0ea10c16ec0ca
gael <gael.pellevoizin@davidson.fr>
2021-04-12 11:20:35 +0200

    Supression module css qui sera finallement Car sonar ne permet pu de faire de plugin "java" pour le css. On intégrera donc un linter css (stylelint) comme analyseur externe pour analyser le CSS. https://docs.sonarqube.org/latest/extend/adding-coding-rules/

--- css-plugin/css-bundle/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  collectCoverageFrom: ["src/**/*.ts"],
-  moduleFileExtensions: ["js", "ts"],
-  testResultsProcessor: "jest-sonar-reporter",
-  transform: {
-    "^.+\\.ts$": "ts-jest"
-  },
-  testMatch: ["<rootDir>/tests/**/*.test.ts"]
-};
--- css-plugin/src/main/java/fr/cnumr/css/MyLessCustomRulesDefinition.java
+++
@@ -1,4 +0,0 @@
-package fr.cnumr.css;
-
-public class MyLessCustomRulesDefinition {
-}
