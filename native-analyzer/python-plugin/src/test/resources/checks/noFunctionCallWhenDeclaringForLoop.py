c14fd05fbb368cd881b66149f5c0f17e93d7080a
Gaël Pellevoizin <gael.pellevoizin@davidson.fr>
2021-04-16 13:31:21 +0200

    Correction test S69 python

--- native-analyzer/python-plugin/src/test/resources/checks/noFunctionCallWhenDeclaringForLoop.py
+++ native-analyzer/python-plugin/src/test/resources/checks/noFunctionCallWhenDeclaringForLoop.py
@@ -1,7 +1,7 @@
 def my_function():
     return 6
 
-for i in my_function(): # Noncompliant {{For statement.}}
+for i in my_function(): # Noncompliant {{Ne pas appeler de fonction dans la déclaration d’une boucle de type for}}
     print("Test")
     my_function()
     pass
